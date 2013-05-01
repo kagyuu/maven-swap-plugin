a72c786a6f00d1761f3acbbe14bb6a3677412072
atsushi <atsushi@192.168.1.133>
2013-05-02 02:00:54 +0900

    Change Group ID. Becase Sonatype says we should use domain name as group id.

--- src/test/java/jp/hondoh/maven/swap/plugin/SwapMojoTest.java
+++ src/test/java/jp/hondoh/maven/swap/plugin/SwapMojoTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 HONDOH Atsushi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jp.hondoh.maven.swap.plugin;
 
 import java.io.BufferedReader;
@@ -17,13 +32,14 @@ import static org.hamcrest.CoreMatchers.equalTo;
  */
 public class SwapMojoTest extends AbstractMojoTestCase {
 
+    private static final String GOAL = "swapfile";
     @Test
     public void testExecuteSwapEjbPersistenceXml() throws Exception {
         String main1 = getContents(new File(getBasedir(), "target/classes/META-INF/persistence.xml"));
         String test1 = getContents(new File(getBasedir(), "target/test-classes/META-INF/persistence.xml"));
         
         File testPom = new File(getBasedir(), "src/test/resources/pom_test1.xml");
-        SwapMojo mojo = (SwapMojo) lookupMojo("swapfiles", testPom);
+        SwapMojo mojo = (SwapMojo) lookupMojo(GOAL, testPom);
         mojo.execute();
         
         String main2 = getContents(new File(getBasedir(), "target/classes/META-INF/persistence.xml"));
@@ -40,7 +56,7 @@ public class SwapMojoTest extends AbstractMojoTestCase {
         String test1 = getContents(new File(getBasedir(), "target/test-classes/META-INF/persistence.xml"));
         
         File testPom = new File(getBasedir(), "src/test/resources/pom_test2.xml");
-        SwapMojo mojo = (SwapMojo) lookupMojo("swapfiles", testPom);
+        SwapMojo mojo = (SwapMojo) lookupMojo(GOAL, testPom);
         mojo.execute();
         
         String main2 = getContents(new File(getBasedir(), "target/classes/META-INF/persistence.xml"));
@@ -57,7 +73,7 @@ public class SwapMojoTest extends AbstractMojoTestCase {
         String test1 = getContents(new File(getBasedir(), "target/test-classes/META-INF/persistence.xml"));
         
         File testPom = new File(getBasedir(), "src/test/resources/pom_test3.xml");
-        SwapMojo mojo = (SwapMojo) lookupMojo("swapfiles", testPom);
+        SwapMojo mojo = (SwapMojo) lookupMojo(GOAL, testPom);
         mojo.execute();
         
         String main2 = getContents(new File(getBasedir(), "target/classes/META-INF/persistence.xml"));
